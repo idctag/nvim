e7a4c58890ac1e919bc6d4b62348693eb4da3925
ochir <ochir.b@tanasoft.mn>
2025-08-07 12:37:22 +0800

    init

---
+++ lua/community.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.full-dadbod" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.pack.biome" },
+}
